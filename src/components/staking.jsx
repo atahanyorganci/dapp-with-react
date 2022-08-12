b0218fd33f767db6a1e37bedf63b41d3fcbdff88
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-08-13 01:22:16 +0300

    Add stake and withdraw functionality

--- src/components/staking.jsx
+++ src/components/staking.jsx
@@ -22,6 +22,30 @@ const getStakingViews = async (account, provider) => {
 
 const Staking = ({ account, provider }) => {
   const [views, setViews] = useState({});
+  const [stake, setStake] = useState("");
+  const [withdraw, setWithdraw] = useState("");
+
+  const handleStake = async event => {
+    event.preventDefault();
+    const signer = provider.getSigner(account);
+    const staking = STAKING_CONTRACT.connect(signer);
+
+    const tx = await staking.stake(ethers.utils.parseEther(stake), {
+      gasLimit: 1_000_000,
+    });
+    await tx.wait();
+  };
+
+  const handleWithdraw = async event => {
+    event.preventDefault();
+    const signer = provider.getSigner(account);
+    const staking = STAKING_CONTRACT.connect(signer);
+
+    const tx = await staking.withdraw(ethers.utils.parseEther(withdraw), {
+      gasLimit: 1_000_000,
+    });
+    await tx.wait();
+  };
 
   useEffect(() => {
     getStakingViews(account, provider).then(setViews).catch(console.error);
@@ -48,6 +72,28 @@ const Staking = ({ account, provider }) => {
       <p>
         <strong>Total Staked: </strong> {views.totalStaked} ATA
       </p>
+      <div style={{ display: "flex" }}>
+        <form onSubmit={handleStake}>
+          <label htmlFor="stake">Stake</label>
+          <input
+            id="stake"
+            placeholder="0.0 ATA"
+            value={stake}
+            onChange={e => setStake(e.target.value)}
+          />
+          <button type="submit">Stake ATA</button>
+        </form>
+        <form onSubmit={handleWithdraw}>
+          <label htmlFor="withdraw">Withdraw</label>
+          <input
+            id="withdraw"
+            placeholder="0.0 ATA"
+            value={withdraw}
+            onChange={e => setWithdraw(e.target.value)}
+          />
+          <button type="submit">Withdraw ATA</button>
+        </form>
+      </div>
     </div>
   );
 };
