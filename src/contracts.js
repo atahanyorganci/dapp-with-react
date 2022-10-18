71cebf5e5b842711412c9e2450d175a1ef8802b7
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-10-18 16:38:29 +0200

    Update contract addresses

--- src/contracts.js
+++ src/contracts.js
@@ -2,10 +2,10 @@ import { ethers } from "ethers";
 import AtaTokenABI from "../abi/ataToken.abi.json";
 import StakingAbi from "../abi/staking.abi.json";
 
-export const ATA_TOKEN_ADDRESS = "0x47741C56064B4c129d585CEe9A9d3A3dB7CDA9ce";
+export const ATA_TOKEN_ADDRESS = "0x5E8F49F4062d3a163cED98261396821ae2996596";
 export const ATA_TOKEN = new ethers.Contract(ATA_TOKEN_ADDRESS, AtaTokenABI);
 
-export const STAKING_ADDRESS = "0x2Fea511D355e7A6f8A1F9BE566025a19762B9138";
+export const STAKING_ADDRESS = "0xAC1BdE0464D932bf1097A9492dCa8c3144194890";
 export const STAKING_CONTRACT = new ethers.Contract(
   STAKING_ADDRESS,
   StakingAbi
