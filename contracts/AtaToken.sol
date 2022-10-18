1a1c59d4b540a7ac7f6cffaae366ecabda38c28f
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-10-18 16:33:34 +0200

    Update `AtaToken` make `rewardAmount` constant

--- contracts/AtaToken.sol
+++ contracts/AtaToken.sol
@@ -4,20 +4,19 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AtaToken is ERC20 {
-  mapping(address => uint8) claimants;
-  uint256 public immutable rewardAmount;
-
   uint8 constant CLAIM_STATUS_PENDING = 0;
   uint8 constant CLAIM_STATUS_CLAIMED = 1;
+  uint256 constant  REWARD_AMOUNT = 1000 * 10 ** 18;
+
+  mapping(address => uint8) claimants;
 
   constructor() ERC20("AtaToken", "ATA") {
     _mint(msg.sender, 1000000 * 10**decimals());
-    rewardAmount = 1000 * 10**decimals();
   }
 
   function claim() public {
     require(!hasClaimed(msg.sender), "Already claimed");
-    _mint(msg.sender, rewardAmount);
+    _mint(msg.sender, REWARD_AMOUNT);
     claimants[msg.sender] = CLAIM_STATUS_CLAIMED;
   }
 
