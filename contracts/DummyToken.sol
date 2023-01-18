96a0637a0cb126bd074be9b2aec45b2881408aae
Atahan Yorgancı <atahanyorganci@hotmail.com>
2023-01-18 03:20:51 +0300

    feat: make `DummyToken` mintable by the owner

--- contracts/DummyToken.sol
+++ contracts/DummyToken.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DummyToken is ERC20 {
+contract DummyToken is ERC20, Ownable {
   uint8 constant CLAIM_STATUS_PENDING = 0;
   uint8 constant CLAIM_STATUS_CLAIMED = 1;
   uint256 constant REWARD_AMOUNT = 1000 * 10 ** 18;
@@ -12,6 +13,10 @@ contract DummyToken is ERC20 {
 
   constructor() ERC20("DummyToken", "DT") {
     _mint(msg.sender, 1_000_000 * 10 ** 18);
+  }
+
+  function mint(address _account, uint256 _amount) public onlyOwner {
+    _mint(_account, _amount);
   }
 
   function claim() public {
