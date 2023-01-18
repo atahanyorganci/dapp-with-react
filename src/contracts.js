a687763826d5cfe27b2c5fa763a21f0055f77b72
Atahan Yorgancı <atahanyorganci@hotmail.com>
2023-01-19 01:04:16 +0300

    refactor: access contract address from `import.meta.env`

--- src/contracts.js
+++ src/contracts.js
@@ -2,13 +2,13 @@ import { ethers } from "ethers";
 import DummyTokenABI from "../abi/dummyToken.abi.json";
 import StakingAbi from "../abi/stakingVault.abi.json";
 
-export const DUMMY_TOKEN_ADDRESS = "0x5E8F49F4062d3a163cED98261396821ae2996596";
+export const DUMMY_TOKEN_ADDRESS = import.meta.env.VITE_DUMMY_TOKEN_ADDRESS;
 export const DUMMY_TOKEN = new ethers.Contract(
   DUMMY_TOKEN_ADDRESS,
   DummyTokenABI
 );
 
-export const STAKING_ADDRESS = "0xAC1BdE0464D932bf1097A9492dCa8c3144194890";
+export const STAKING_ADDRESS = import.meta.env.VITE_STAKING_ADDRESS;
 export const STAKING_CONTRACT = new ethers.Contract(
   STAKING_ADDRESS,
   StakingAbi
